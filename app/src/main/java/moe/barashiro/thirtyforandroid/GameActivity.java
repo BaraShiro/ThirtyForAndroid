982d52f1ba674c1d5567e5871bdb7e54874ba0ce
Robert Rosborg <rosborg.robert@gmail.com>
2019-07-04 11:49:35 +0200

    MethodSpinner working

--- app/src/main/java/moe/barashiro/thirtyforandroid/GameActivity.java
+++ app/src/main/java/moe/barashiro/thirtyforandroid/GameActivity.java
@@ -6,22 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseIntArray;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameActivity extends AppCompatActivity {
 
-    private SparseIntArray whiteDices;
-    private SparseIntArray redDices;
-    private boolean[] diceMarkedForReroll;
-
+    private SparseIntArray mWhiteDices;
+    private SparseIntArray mRedDices;
+    private boolean[] mDiceMarkedForReroll;
     private Game mGame;
+
     private ImageButton[] mDiceButtons;
-    private Button mReroll;
-    private TextView mRerollsLeft;
-    private TextView mRound;
-    private Button mCalculate;
+    private TextView mRerollText;
+    private Button mRerollButton;
+    private TextView mRerollsLeftText;
+    private TextView mRoundText;
+    private TextView mScoreText;
+    private Spinner mMethodSpinner;
+    private Button mCalculateButton;
     private Button mNext;
 
     public static Intent newIntent(Context packageContext) {
@@ -34,26 +42,30 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        whiteDices = new SparseIntArray(6);
-        whiteDices.put(1, R.drawable.white1);
-        whiteDices.put(2, R.drawable.white2);
-        whiteDices.put(3, R.drawable.white3);
-        whiteDices.put(4, R.drawable.white4);
-        whiteDices.put(5, R.drawable.white5);
-        whiteDices.put(6, R.drawable.white6);
+        mWhiteDices = new SparseIntArray(6);
+        mWhiteDices.put(1, R.drawable.white1);
+        mWhiteDices.put(2, R.drawable.white2);
+        mWhiteDices.put(3, R.drawable.white3);
+        mWhiteDices.put(4, R.drawable.white4);
+        mWhiteDices.put(5, R.drawable.white5);
+        mWhiteDices.put(6, R.drawable.white6);
 
-        redDices = new SparseIntArray(6);
-        redDices.put(1, R.drawable.red1);
-        redDices.put(2, R.drawable.red2);
-        redDices.put(3, R.drawable.red3);
-        redDices.put(4, R.drawable.red4);
-        redDices.put(5, R.drawable.red5);
-        redDices.put(6, R.drawable.red6);
-
-        diceMarkedForReroll = new boolean[] {false, false, false, false, false, false};
+        mRedDices = new SparseIntArray(6);
+        mRedDices.put(1, R.drawable.red1);
+        mRedDices.put(2, R.drawable.red2);
+        mRedDices.put(3, R.drawable.red3);
+        mRedDices.put(4, R.drawable.red4);
+        mRedDices.put(5, R.drawable.red5);
+        mRedDices.put(6, R.drawable.red6);
 
         mGame = new Game();
+        mDiceMarkedForReroll = new boolean[] {false, false, false, false, false, false};
 
+        mRerollButton = (Button) findViewById(R.id.rerollButton);
+        mRerollText = (TextView) findViewById(R.id.rerollText);
+        mScoreText = (TextView) findViewById(R.id.scoreText);
+        mMethodSpinner = (Spinner) findViewById(R.id.methodSpinner);
+        mCalculateButton = (Button) findViewById(R.id.calculateButton);
         mDiceButtons = new ImageButton[] {
                 (ImageButton) findViewById(R.id.diceOneButton),
                 (ImageButton) findViewById(R.id.diceTwoButton),
@@ -62,10 +74,15 @@ public class GameActivity extends AppCompatActivity {
                 (ImageButton) findViewById(R.id.diceFiveButton),
                 (ImageButton) findViewById(R.id.diceSixButton)
         };
-        mReroll = (Button) findViewById(R.id.reroll);
-        mReroll.setEnabled(false);
 
+
+        mRerollButton.setEnabled(false);
         updateAllDice();
+        updateRerollText();
+        updateScoreText(0);
+        updateMethodSpinner();
+
+
 
 
         mDiceButtons[0].setOnClickListener(new View.OnClickListener() {
@@ -110,13 +127,14 @@ public class GameActivity extends AppCompatActivity {
             }
         });
 
-        mReroll.setOnClickListener(new View.OnClickListener() {
+        mRerollButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                mGame.rerollDices(diceMarkedForReroll[0], diceMarkedForReroll[1],diceMarkedForReroll[2],diceMarkedForReroll[3],diceMarkedForReroll[4],diceMarkedForReroll[5]);
+                mGame.rerollDices(mDiceMarkedForReroll[0], mDiceMarkedForReroll[1], mDiceMarkedForReroll[2], mDiceMarkedForReroll[3], mDiceMarkedForReroll[4], mDiceMarkedForReroll[5]);
                 updateAllDice();
-                mReroll.setEnabled(false);
+                updateRerollText();
+                mRerollButton.setEnabled(false);
             }
         });
 
@@ -125,18 +143,18 @@ public class GameActivity extends AppCompatActivity {
     private void markDice(int dice)
     {
         if(mGame.getRerollsLeft() > 0) {
-            if (diceMarkedForReroll[dice]) {
-                diceMarkedForReroll[dice] = false;
-                updateDiceButtonGraphics(mDiceButtons[dice], whiteDices, mGame.getDiceValue(dice));
+            if (mDiceMarkedForReroll[dice]) {
+                mDiceMarkedForReroll[dice] = false;
+                updateDiceButtonGraphics(mDiceButtons[dice], mWhiteDices, mGame.getDiceValue(dice));
             } else {
-                diceMarkedForReroll[dice] = true;
-                updateDiceButtonGraphics(mDiceButtons[dice], redDices, mGame.getDiceValue(dice));
+                mDiceMarkedForReroll[dice] = true;
+                updateDiceButtonGraphics(mDiceButtons[dice], mRedDices, mGame.getDiceValue(dice));
             }
 
             if(someDiceAreMarked()){
-                mReroll.setEnabled(true);
+                mRerollButton.setEnabled(true);
             } else{
-                mReroll.setEnabled(false);
+                mRerollButton.setEnabled(false);
             }
 
         }
@@ -146,8 +164,8 @@ public class GameActivity extends AppCompatActivity {
     private void updateAllDice(){
         int[] diceValues = mGame.getDiceValues();
         for (int i = 0; i < 6; i++){
-            updateDiceButtonGraphics(mDiceButtons[i], whiteDices, diceValues[i]);
-            diceMarkedForReroll[i] = false;
+            updateDiceButtonGraphics(mDiceButtons[i], mWhiteDices, diceValues[i]);
+            mDiceMarkedForReroll[i] = false;
         }
 
     }
@@ -155,11 +173,66 @@ public class GameActivity extends AppCompatActivity {
     private boolean someDiceAreMarked(){
         boolean marked = false;
         for (int i = 0; i < 6; i++){
-            if(diceMarkedForReroll[i]){
+            if(mDiceMarkedForReroll[i]){
                 marked = true;
             }
         }
         return marked;
+    }
+
+    private void updateRerollText(){
+        String newRerollText = getResources().getString(R.string.rerollText) + " " + mGame.getRerollsLeft();
+        mRerollText.setText(newRerollText);
+    }
+
+    private void updateScoreText(int score){
+        String newScoreText = getResources().getString(R.string.scoreText) + " " + score;
+        mScoreText.setText(newScoreText);
+    }
+
+    private void updateMethodSpinner(){
+        ArrayAdapter<String> methodAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, createMethodSpinnerList());
+        methodAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mMethodSpinner.setAdapter(methodAdapter);
+    }
+
+    private List<String> createMethodSpinnerList(){
+        List<String> list = new ArrayList<String>();
+        boolean[] methodsUsed = mGame.getScoreMethodsUsed();
+        String[] values = getResources().getStringArray(R.array.methodArray);
+        for(int i = 0; i < 10; i++){
+            if(!methodsUsed[i]){
+                list.add(values[i]);
+            }
+        }
+        return list;
+    }
+
+    private int methodStringToInt(String method){
+        switch(method) {
+            case "Low":
+                return 0;
+            case "Fours":
+                return 1;
+            case "Fives":
+                return 2;
+            case "Sixes":
+                return 3;
+            case "Sevens":
+                return 4;
+            case "Eight":
+                return 5;
+            case "Nines":
+                return 6;
+            case "Tens":
+                return 7;
+            case "Elevens":
+                return 8;
+            case "Twelves":
+                return 9;
+        }
+        return 0;
     }
 
     private void updateDiceButtonGraphics(ImageButton diceButton, SparseIntArray diceGraphics, int diceValue){
